760c27187904b0b91dff54905b32619001e56015
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-02-11 14:11:55 +0100

    Added testcase for Lucky

---
+++ tests/AppBundle/Controller/LuckyControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LuckyControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/lucky/number');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Lucky number', $crawler->filter('body')->text());
+    }
+}
