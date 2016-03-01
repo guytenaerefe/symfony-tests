df13876d9b3ad025f4d983777cc138875902025a
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-01 16:57:00 +0100

    Added minor functions to see PHPUnit coverage differences

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -1,18 +1,39 @@
 <?php namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class LuckyController
 {
     /**
+     * @return int
+     */
+    private function number()
+    {
+        return rand(0, 100);
+    }
+
+    /**
      * @Route("/lucky/number")
      */
     public function numberAction()
     {
-        $number = rand(0, 100);
+        $number = $this->number();
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
         );
     }
+
+    /**
+     * @Route("/api/lucky/number")
+     */
+    public function apiNumberaction()
+    {
+        $data = [
+            'lucky_number' => $this->number(),
+        ];
+        return new JsonResponse($data);
+    }
+
 }
