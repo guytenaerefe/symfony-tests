1e1dd6d0cd49204134dfe8ea1f338aede6126fff
Francesco de Guytenaere <guytenaere.fe@gmail.com>
2016-03-09 00:29:45 +0100

    Oops. Turns out they were in use by annotations.

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -1,5 +1,6 @@
 <?php namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
